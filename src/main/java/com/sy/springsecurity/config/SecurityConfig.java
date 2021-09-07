52695a9cef8f40457657d13995b99eba7e685799
sunyubk <1870971964@qq.com>
2021-09-07 20:12:54 +0800

    增加一个注解

--- src/main/java/com/sy/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/sy/springsecurity/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
@@ -30,6 +31,7 @@ import java.util.Set;
  * @Version 1.0
  **/
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
